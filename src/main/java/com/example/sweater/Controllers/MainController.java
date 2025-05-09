f7385d1baf6db797eb241cba3bf07690a9faee53
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-09 10:54:40 +0300

    Lesson 2 DB Добавляются и отображаются сообщения

--- src/main/java/com/example/sweater/Controllers/MainController.java
+++ src/main/java/com/example/sweater/Controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,11 +15,21 @@ public class MainController {
     @Autowired
     public MessageRepo messageRepo;
 
-    @GetMapping("/main")
-//    public String main(@RequestParam String text, @RequestParam String tag, Model model) {
+    @GetMapping("/")
     public String main(Model model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
         return "main";
     }
+
+    @PostMapping("/")
+    public String addMessage(Model model, @RequestParam String text, @RequestParam String tag) {
+        messageRepo.save(new Message(text, tag));
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+
+    return "main";
+    }
+
 }
