8d32eaf48956f8577cd85c9c946da90cc91938ef
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-18 19:13:47 +0300

    Lesson 6 работает корректировка пользователя

--- src/main/java/com/example/sweater/Controllers/UserController.java
+++ src/main/java/com/example/sweater/Controllers/UserController.java
@@ -6,9 +6,12 @@ import com.example.sweater.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
@@ -27,5 +30,26 @@ public class UserController {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
+    }
+
+    @PostMapping()
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user,
+            Model model) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        user.setUsername(username);
+        userRepo.save(user);
+        return "redirect:/user";
     }
 }
