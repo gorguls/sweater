82d270f4d30a5f6c9f462eb8501230e79a2ec858
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-18 19:21:46 +0300

    Lesson 6 Базовое разграничение доступа

--- src/main/java/com/example/sweater/Controllers/UserController.java
+++ src/main/java/com/example/sweater/Controllers/UserController.java
@@ -4,6 +4,7 @@ import com.example.sweater.Domain.Role;
 import com.example.sweater.Domain.User;
 import com.example.sweater.Repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepo userRepo;
