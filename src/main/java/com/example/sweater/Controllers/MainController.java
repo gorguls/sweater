c6809c283d3bb12e953a335ee894dbb93342b82f
Сергей Елисеев <yeliseyev.serg@gmail.com>
2025-05-21 20:20:36 +0300

    Lesson 7 Добавления в контроллер

--- src/main/java/com/example/sweater/Controllers/MainController.java
+++ src/main/java/com/example/sweater/Controllers/MainController.java
@@ -4,15 +4,24 @@ import com.example.sweater.Domain.Message;
 import com.example.sweater.Domain.User;
 import com.example.sweater.Repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @Autowired
     public MessageRepo messageRepo;
@@ -46,17 +55,30 @@ public class MainController {
             @AuthenticationPrincipal User user,
             @RequestParam String text,
             @RequestParam String tag,
+            @RequestParam("file") MultipartFile file,
             @RequestParam(required = false) String filter,
-            Model model) {
+            Model model) throws IOException {
 
-        Iterable<Message> messages;
+        Message message = new Message(text,tag,user);
 
-        if (text != null && !text.isEmpty()) {
-            messageRepo.save(new Message(text, tag, user));
+        if (file != null && !file.isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            uuidFile+="."+file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + uuidFile));
+
+            message.setFilename(uuidFile);
         }
 
-        messages = messageRepo.findAll();
-        model.addAttribute("messages", messages);
+        if (text != null && !text.isEmpty()) {
+            messageRepo.save(message);
+        }
+
+        model.addAttribute("messages", messageRepo.findAll());
 
         model.addAttribute("filter", "");
 
